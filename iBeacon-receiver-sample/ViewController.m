45d791a472ca05edadf0f9d55e50d0d940cc4231
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-10-26 19:02:54 +0900

    init

--- iBeacon-receiver-sample/ViewController.m
+++ iBeacon-receiver-sample/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController ()<CLLocationManagerDelegate>
+
+@property (strong, nonatomic) NSUUID *proximityUUID;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLBeaconRegion *beaconRegion;
 
 @end
 
@@ -16,12 +21,90 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // iBeacon受信に関する初期設定
+    if ([CLLocationManager isMonitoringAvailableForClass:[CLBeaconRegion class]]) {
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.delegate = self;
+        
+        NSString *uuid = @"YOUR_DEVICE'S_UUID";
+        self.proximityUUID = [[NSUUID alloc] initWithUUIDString:uuid];
+        self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:self.proximityUUID identifier:@"com.shoya140.iBeacon-receiver-sample"];
+        self.beaconRegion.notifyOnEntry = YES;
+        self.beaconRegion.notifyOnExit = YES;
+        self.beaconRegion.notifyEntryStateOnDisplay = YES;
+        
+        [self.locationManager startMonitoringForRegion:self.beaconRegion];
+    }
+    
+    // 通知についての許可
+    UIUserNotificationType types = UIUserNotificationTypeBadge|UIUserNotificationTypeSound|UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)sendNotification:(NSString *)message
+{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:0];
+    notification.timeZone = [NSTimeZone defaultTimeZone];
+    notification.alertBody = message;
+    notification.alertAction = @"Open";
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    // アプリを起動していない間もiBeaconの電波を探し続ける許可
+    if (status == kCLAuthorizationStatusNotDetermined) {
+        [manager requestAlwaysAuthorization];
+    }
+}
+
+#pragma mark - CLLocationManagerDelegate methods
+
+- (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
+{
+    [self.locationManager requestStateForRegion:self.beaconRegion];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region
+{
+    switch (state) {
+        case CLRegionStateInside:
+            if ([region isMemberOfClass:[CLBeaconRegion class]] && [CLLocationManager isRangingAvailable]) {
+                [self.locationManager startRangingBeaconsInRegion:self.beaconRegion];
+            }
+            break;
+        case CLRegionStateOutside:
+            if ([region isMemberOfClass:[CLBeaconRegion class]] && [CLLocationManager isRangingAvailable]) {
+                [self.locationManager stopRangingBeaconsInRegion:self.beaconRegion];
+            }
+            break;
+        case CLRegionStateUnknown:
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
+{
+    // 起動時にiBeaconのエリアに入っている場合は呼ばれないことに注意
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    [self sendNotification:@"didEnterRegion"];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
+{
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    [self sendNotification:@"didExitRegion"];
 }
 
 @end
